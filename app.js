a4f3a73493814c7011bcdeffa4fa3645e695c57d
Thomas Williams <cree83@msn.com>
2022-11-12 12:05:22 -0500

    completed assignment

--- app.js
+++ app.js
@@ -2,3 +2,92 @@ console.log("Hello World!\n==========\n");
 
 // PROJECT Section
 console.log("PROJECT:\n==========\n");
+
+class Book {
+    id;
+    title;
+    author;
+    isRead;
+    
+    constructor (title, author, read){
+
+        this.title = title;
+        this.author = author;
+        this.isRead = read;
+    }
+}
+
+class Library {
+    bookCount;
+    books;
+    
+
+    constructor(count, array){
+        this.bookCount = count;
+        this.books = array;
+
+    }
+    
+    markRead(checkbox, id){
+       this.books.forEach((book) => {
+            if (book.id == id){
+                book.isRead = true;
+                checkbox.checked = true;
+                checkbox.disabled = true;
+            }
+       }) 
+    }
+
+    addBook(){
+        //Selections
+        const getTitle = document.querySelector("#input-title");
+        const getAuthor = document.querySelector("#input-author");
+        const getReadBook = document.querySelector("#input-readBook");
+        const getTableBody = document.querySelector("#booksList");
+
+        const book = new Book(getTitle.value, getAuthor.value, getReadBook.checked);
+        this.books.push(book);
+
+        //Created Elements
+        const tableRow = document.createElement("tr");
+        const tableTitle = document.createElement("td");
+        tableTitle.textContent = book.title;
+        const tableAuthor = document.createElement("td");
+        tableAuthor.textContent = book.author;
+        const tableIsRead = document.createElement("td");
+        //tableIsRead.textContent = book.isRead;
+
+        const inputIsRead = document.createElement("input");
+        inputIsRead.type = "checkbox";
+        inputIsRead.checked = book.isRead;
+        inputIsRead.disabled = true;
+
+        //Added Elements
+        tableIsRead.append(inputIsRead);
+        tableRow.append(tableTitle);
+        tableRow.append(tableAuthor);
+        tableRow.append(tableIsRead);
+        getTableBody.append(tableRow);
+
+        this.bookCount++;
+
+        getTitle.value = "";
+        getAuthor.value = "";
+        getReadBook.checked = false;
+    }
+}
+
+const addToLibrary = new Library(1, [new Book("Name of the Wind", "Patrick Rothfuss", true)]);
+
+const addBookBtn = document.querySelector("#addBookBtn");
+addBookBtn.addEventListener("submit", (event) => {
+    event.preventDefault();
+    addToLibrary.addBook();
+})
+
+
+
+
+
+
+
